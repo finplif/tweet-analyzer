4a0063c0bf5ec23f2ca54a8273751723aaa85848
finplif <42929440+finplif@users.noreply.github.com>
2022-12-16 20:30:08 -0600

    Add files via upload

--- tweet analyzer.py
+++ tweet analyzer.py
@@ -6,9 +6,8 @@ import re
 class TwitterAnalyzer:
     def __init__(self, twitter_file):
         self.file = twitter_file
-        self.filter_by_deleted_and_published()
-
-
+        self.published, self.deleted = self.filter_by_deleted_and_published()
+        self.topn_languages()
 
     def filter_by_deleted_and_published(self):
 
@@ -18,91 +17,76 @@ class TwitterAnalyzer:
 
         published = []
         deleted = []
-        
+
         for i in range(len(twitter)):
             if 'delete' in twitter[i].keys():
                 deleted.append(twitter[i])
             else:
                 published.append(twitter[i])
-                
-        self.published = published
-        self.deleted = deleted
-   
 
+        return published, deleted
 
-    def get_top_20_languages(self):
-
+    def topn_languages(self, topn=20):
         language = []
+        lang_dic = {}
         for i in range(len(self.published)):
             language.append(self.published[i]['lang'])
-        list_of_languages = Counter(language)
-       
-        return list_of_languages
-    
+        languages_total = Counter(language)
+        top_languages = languages_total.most_common(topn)
+        for i in range(len(top_languages)):
+            lang_dic[top_languages[i][0]] = top_languages[i][1]
 
+        return lang_dic
 
     def serial_twitters(self):
         humans = []
         for i in range(len(self.published)):
             humans.append(self.published[i]['user']['screen_name'])
         list_of_users = Counter(humans)
-       
+
         dictionary_of_users = {}
         dictionary_of_users.update(list_of_users)
         quantity = 0
         for user in dictionary_of_users:
-            if dictionary_of_users [user] > 1:
+            if dictionary_of_users[user] > 1:
                 quantity += 1
-       
-        return quantity
-    
 
-   
-    def top_10_hashtags(self):
-        
+        return quantity
+
+    def topn_hashtags(self, topn=10):
         hashtags = []
+        hashtags_dic = {}
         for i in range(len(self.published)):
             hashtag = self.published[i]['entities']['hashtags']
             if len(hashtag) != 0:
                 for j in range(len(hashtag)):
                     hashtags.append(hashtag[j]['text'])
-        list_of_hashtags = Counter(hashtags)
-       
-        return list_of_hashtags
+        hashtags_total = Counter(hashtags)
+        top_hashtags = hashtags_total.most_common(topn)
+        for i in range(len(top_hashtags)):
+            hashtags_dic[top_hashtags[i][0]] = top_hashtags[i][1]
 
+        return hashtags_dic
 
-
-    def frequency_dictionary(self):
-       
-        own = []
+    def frequency_dictionary(self, topn=20):
+        personal_tweets = []
+        tweets_dic = {}
         for i in range(len(self.published)):
             if 'retweeted_status' not in self.published[i] and self.published[i]['lang'] == 'en':
-                own.append(self.published[i]['text'])
-           
-        own1 = ''.join(own)
-        own1 = re.sub(r'[^\w\s]', '', own1)
-        own1 = own1.lower().split()
-        list_of_words = Counter(own1)
-       
-        return list_of_words
-   
+                personal_tweets.append(self.published[i]['text'])
 
-
-analyzer = TwitterAnalyzer('twitter.json')
-    
-list_of_languages = analyzer.get_top_20_languages()
-
-quantity = analyzer.serial_twitters()
-
-list_of_hashtags = analyzer.top_10_hashtags()
-
-list_of_words = analyzer.frequency_dictionary()
-   
-print(quantity)
+        cleaned_tweets = ''.join(personal_tweets)
+        cleaned_tweets = re.sub(r'[^\w\s]', '', cleaned_tweets)
+        cleaned_tweets = cleaned_tweets.lower().split()
+        words_total = Counter(cleaned_tweets)
+        frequent_words = words_total.most_common(topn)
+        for i in range(len(frequent_words)):
+            tweets_dic[frequent_words[i][0]] = frequent_words[i][1]
+        return tweets_dic
 
 
 #### how many followers do the tweets authors have? top-10 authors
-   
+
 """
 followers_names = []
 for i in range (len (published)):
@@ -111,4 +95,3 @@ followers_qus = []
 for i in range (len (published)):
     followers_qus.append (published[i]['user']['followers_count'])
 """
-
